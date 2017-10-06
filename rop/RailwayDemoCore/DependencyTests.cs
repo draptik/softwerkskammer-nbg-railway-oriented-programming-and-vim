fec2f68d11a21550752baa3e666b154d7a3c65fa
Patrick Drechsler <patrick@pdrechsler.de>
2017-10-06 20:01:35 +0200

    checked that all dependencies work with core

---
+++ rop/RailwayDemoCore/DependencyTests.cs
@@ -0,0 +1,34 @@
+using System;
+using CSharpFunctionalExtensions;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace RailwayDemoCore
+{
+    public class DependencyTests
+    {
+        [Fact]
+        public void FluentAssertions_work_with_dotnet_core()
+        {
+            true.Should().BeTrue();
+        }
+
+        [Fact]
+        public void NSubstitute_works_with_dotnet_core()
+        {
+            Substitute.For<IFoo>().Blub("test").Returns("test");
+        }
+
+        [Fact]
+        public void CSharpFunctionalExtensions_work_with_dotnet_core()
+        {
+            Result.Ok().IsSuccess.Should().BeTrue();
+        }
+    }
+
+    public interface IFoo
+    {
+        string Blub(string s);
+    }
+}
--- rop/RailwayDemoCore/README.md
+++ rop/RailwayDemoCore/README.md
@@ -1,4 +1,11 @@
 # Project creation
 
 - `dotnet new xunit -o <project-name>`
+- `cd <project-name>`
 
+# Installing packages
+
+- `dotnet add package FluentAssertions`
+- `dotnet add package NSubstitute`
+- `dotnet add package CSharpFunctionalExtensions`
+- `dotnet restore`
